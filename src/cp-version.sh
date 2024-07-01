52283a42f2567921c51fd95bc427af8a5176f419
BarryChum <barrychum@users.noreply.github.com>
2024-07-01 17:13:54 +0100

    Update cp-version.sh

--- src/cp-version.sh
+++ src/cp-version.sh
@@ -2,38 +2,40 @@
 
 # Check for mandatory arguments
 if [ $# -ne 2 ]; then
+    echo "provide source_file and target_folder" >&2
     exit 1
 fi
-
-# Extract source file and target folder
-source_file="$1"
-target_folder="$2"
 
 # Check if source file exists
-if [ ! -f "$source_file" ]; then
+if [ ! -f "$1" ]; then
+    echo "source file does not exist" >&2
     exit 1
 fi
 
+source_fullname=$(realpath "$1")
+source_filename=$(basename "$source_fullname")
+target_fullfolder=$(realpath "$2")
+
 # Create target folder with .version subfolder if they don't exist
-target_version_folder="$target_folder/.version"
+target_version_folder="$target_fullfolder/.version"
 if [ ! -d "$target_version_folder" ]; then
     mkdir -p "$target_version_folder" || exit 1
 fi
 
-# Get target filename from source filename
-target_file=$(basename "$source_file")
-
 # Check if target file exists and create version
-if [ -f "$target_folder/$target_file" ]; then
+if [ -f "$target_fullfolder/$source_filename" ]; then
     # Get current timestamp in desired format
     timestamp=$(date +%Y%m%d-%H%M%S)
-    cp -a "$target_folder/$target_file" "$target_version_folder/$version_file_name/$source_file.$timestamp.version"
+    cp -a "$target_fullfolder/$source_filename" "$target_version_folder/$source_filename.$timestamp.version"
+    if [ $? -ne 0 ]; then
+        printf "error backing up file.  Abort\n" >&2
+        exit 1
+    fi
 fi
 
-if ! cp -a "$source_file" "$target_folder/$target_file"; then
-    exit 1
+if ! cp -a "$source_fullname" "$target_fullfolder/$source_filename"; then
+    printf "error copy file. Abort\n" >&2
+    exit $?
 fi
-
 # Script successful execution (no echo)
 exit 0
-
